43616f1962a43cd0ec1c7aa69acc919cc1b0719c
Analyn Flores <analynflores@gmail.com>
2016-07-27 23:30:52 +0800

    for debugging

--- src/control/validate-host.js
+++ src/control/validate-host.js
@@ -2,6 +2,8 @@
 var ALLOWED_HOSTS = process.env.ALLOWED_HOSTS || 'localhost';
 
 function execute(clientHost, callback) {
+    console.log(clientHost);
+    console.log(ALLOWED_HOSTS);
     if (ALLOWED_HOSTS.includes(clientHost.split(':')[0])) {
         callback(null, {message: clientHost + ' is allowed.'});
     } else {
