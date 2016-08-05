350e53d160e01a6b9b72a49bc989063d77b22c8f
rickzx98 <rickzx98@gmail.com>
2016-08-06 03:19:41 +0800

    updates

--- src/boundary/security-resource.js
+++ src/boundary/security-resource.js
@@ -3,21 +3,32 @@ var Security = require('./security');
 var SecurityException = require('../control/security-exception');
 var API = process.env.API_NAME || '/api/security/';
 
-module.exports = function(app) {
-    app.get('/', function(req, res) {
+module.exports = function (app) {
+    app.get('/', function (req, res) {
         res.status(200).send({
             domain: process.env.DOMAIN_NAME || 'Security',
             links: {
-                validateHost: 'http://' + req.headers.host + API + 'validate-host',
-                validatePassword: 'http://' + req.headers.host + API + 'validate-password',
-                createUserSession: 'http://' + req.headers.host + API + 'create-user-session',
-                validateSession: 'http://' + req.headers.host + API + 'validate-session'
+                validateHost: {
+                    method: 'GET',
+                    url: 'http://' + req.headers.host + API + 'validate-host'
+                },
+                validatePassword: {
+                    method: 'POST',
+                    url: 'http://' + req.headers.host + API + 'validate-password'
+                },
+                createUserSession: {
+                    method: 'POST',
+                    url: 'http://' + req.headers.host + API + 'create-user-session'
+                },
+                validateSession: {
+                    method: 'GET',
+                    url: 'http://' + req.headers.host + API + 'validate-session/:sessionId'
+                }
             }
         });
     });
-
-    app.get(API + 'validate-host', function(req, res) {
-        Security.validateHost(req.headers.host, function(err, result) {
+    app.get(API + 'validate-host', function (req, res) {
+        Security.validateHost(req.headers.host, function (err, result) {
             if (err) {
                 res.status(403).send(new SecurityException(403));
             } else {
@@ -28,13 +39,11 @@ module.exports = function(app) {
             }
         });
     });
-
-
-    app.post(API + 'validate-password', function(req, res) {
-        Security.validatePassword(req.body, function(err, result) {
+    app.post(API + 'validate-password', function (req, res) {
+        Security.validatePassword(req.body, function (err, result) {
             if (err) {
                 //res.status(401).send(new SecurityException(401));
-                res.status(401).send({message: 'Invalid password.'});
+                res.status(401).send({ message: 'Invalid password.' });
             } else {
                 res.status(200).send({
                     message: 'ok',
@@ -43,23 +52,22 @@ module.exports = function(app) {
             }
         });
     });
-
-    app.post(API + 'create-user-session', function(req, res) {
-        Security.createUserSession(req.body.username, function(err, result) {
+    app.post(API + 'create-user-session', function (req, res) {
+        Security.createUserSession(req.body.username, function (err, result) {
             if (err) {
                 res.status(500).send(err);
             } else {
                 res.status(200).send({
-                    sessionId: result});
+                    sessionId: result
+                });
             }
         });
     });
-
-    app.get(API + 'validate-session/:sessionId', function(req, res) {
-        Security.validateSession(req.params.sessionId, function(err, result) {
+    app.get(API + 'validate-session/:sessionId', function (req, res) {
+        Security.validateSession(req.params.sessionId, function (err, result) {
             if (err) {
                 //res.status(401).send(new SecurityException(401));
-                res.status(401).send({message: 'Invalid session.'});
+                res.status(401).send({ message: 'Invalid session.' });
             } else {
                 res.status(200).send({
                     message: 'ok',
